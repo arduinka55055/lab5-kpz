4d589eaf1b4b997fa9d6556c77bfd722fc210262
Denis (PC) <denistesla2005@gmail.com>
2024-04-08 00:10:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using UltraVisionCentre;
+
+
+SoftwareCenter softwareCenter = new("216-1");
+softwareCenter.DownloadCode();
+softwareCenter.LicenseCode();
---
+++ UltraVisionCentre/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace UltraVisionCentre
+{
+   public class SoftwareCenter
+   {
+      private string SourceCode { get; set; }
+      private string LicenseID { get; set; }
+
+      public SoftwareCenter(string licenseID)
+      {
+         LicenseID = licenseID;
+
+         //підрубаємо милозвучну
+         Thread.CurrentThread.CurrentCulture = new CultureInfo("uk-UA");
+         Console.OutputEncoding = Encoding.UTF8;
+      }
+
+      public void DownloadCode()
+      {
+         Console.WriteLine("Код було завантажено!");
+      }
+      public void LicenseCode()
+      {
+         Console.WriteLine("ПЗ було ліцензовано!");
+      }
+   }
+}
